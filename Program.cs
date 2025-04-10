219ab73ae6ddba391a55f7bbce8273795343e3c7
avjajak21949 <avjajak21949@gmail.com>
2025-04-10 19:23:42 +0700

    Fix dropdown functionality and clean up Index.cshtml styles

--- Program.cs
+++ Program.cs
@@ -1,26 +1,144 @@
 using System;
-using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
-using Microsoft.AspNetCore.Hosting;
+using Microsoft.AspNetCore.Builder;
+using Microsoft.AspNetCore.Identity;
+using Microsoft.AspNetCore.Identity.UI;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.Hosting;
-using Microsoft.Extensions.Logging;
+using Demo03.Data;
+using Demo03.Services;
+using Microsoft.AspNetCore.SignalR;
+using Demo03.Hubs;
 
-namespace Demo03
+var builder = WebApplication.CreateBuilder(args);
+
+// Add services to the container.
+var connectionString = builder.Configuration.GetConnectionString("DefaultConnection");
+if (string.IsNullOrEmpty(connectionString))
 {
-    public class Program
-    {
-        public static void Main(string[] args)
-        {
-            CreateHostBuilder(args).Build().Run();
-        }
+    throw new InvalidOperationException("Connection string 'DefaultConnection' not found.");
+}
 
-        public static IHostBuilder CreateHostBuilder(string[] args) =>
-            Host.CreateDefaultBuilder(args)
-                .ConfigureWebHostDefaults(webBuilder =>
-                {
-                    webBuilder.UseStartup<Startup>();
-                });
+builder.Services.AddDbContext<ApplicationDbContext>(options =>
+    options.UseSqlServer(connectionString, sqlServerOptionsAction: sqlOptions =>
+    {
+        sqlOptions.EnableRetryOnFailure(
+            maxRetryCount: 5,
+            maxRetryDelay: TimeSpan.FromSeconds(30),
+            errorNumbersToAdd: null);
+        sqlOptions.CommandTimeout(60);
+    }));
+
+builder.Services.AddDefaultIdentity<IdentityUser>(options => {
+    options.SignIn.RequireConfirmedAccount = true;
+    options.SignIn.RequireConfirmedEmail = true;
+    options.Password.RequireDigit = false;
+    options.Password.RequireLowercase = false;
+    options.Password.RequireNonAlphanumeric = false;
+    options.Password.RequireUppercase = false;
+    options.Password.RequiredLength = 6;
+})
+    .AddRoles<IdentityRole>()
+    .AddEntityFrameworkStores<ApplicationDbContext>();
+
+builder.Services.AddTransient<IEmailService, EmailService>();
+builder.Services.AddControllersWithViews();
+builder.Services.AddRazorPages();
+
+// Add SignalR
+builder.Services.AddSignalR();
+
+// Add CORS
+builder.Services.AddCors(options =>
+{
+    options.AddDefaultPolicy(builder =>
+    {
+        builder.WithOrigins("https://localhost:5001", "http://localhost:5000")
+               .AllowAnyHeader()
+               .AllowAnyMethod()
+               .AllowCredentials();
+    });
+});
+
+var app = builder.Build();
+
+// Configure the HTTP request pipeline.
+if (app.Environment.IsDevelopment())
+{
+    app.UseDeveloperExceptionPage();
+    app.UseMigrationsEndPoint();
+}
+else
+{
+    app.UseExceptionHandler("/Home/Error");
+    app.UseHsts();
+}
+
+app.UseHttpsRedirection();
+app.UseStaticFiles();
+
+app.UseRouting();
+
+// Use CORS
+app.UseCors();
+
+app.UseAuthentication();
+app.UseAuthorization();
+
+app.MapControllerRoute(
+    name: "default",
+    pattern: "{controller=Home}/{action=Index}/{id?}");
+app.MapRazorPages();
+
+// Add SignalR hub endpoint
+app.MapHub<ChatHub>("/chatHub");
+
+// Seed admin user
+await SeedAdminUser(app);
+
+app.Run();
+
+static async Task SeedAdminUser(WebApplication app)
+{
+    using (var scope = app.Services.CreateScope())
+    {
+        var userManager = scope.ServiceProvider.GetRequiredService<UserManager<IdentityUser>>();
+        var roleManager = scope.ServiceProvider.GetRequiredService<RoleManager<IdentityRole>>();
+
+        // Admin account information
+        var adminEmail = "admin@gmail.com";
+        var adminPassword = "Anderson210402@";
+
+        // Check if admin account exists
+        var adminUser = await userManager.FindByEmailAsync(adminEmail);
+        if (adminUser == null)
+        {
+            // Create admin account
+            var newAdmin = new IdentityUser
+            {
+                Id = "+a6s2da+s62da+s62da+s62da+s6d2as+6d2s+3fdg2+",
+                UserName = adminEmail,
+                Email = adminEmail,
+                NormalizedUserName = adminEmail.ToUpper(),
+                NormalizedEmail = adminEmail.ToUpper(),
+                EmailConfirmed = true
+            };
+
+            var createAdminResult = await userManager.CreateAsync(newAdmin, adminPassword);
+
+            if (createAdminResult.Succeeded)
+            {
+                // Assign admin role
+                await userManager.AddToRoleAsync(newAdmin, "Adminstator");
+            }
+            else
+            {
+                var errors = string.Join(", ", createAdminResult.Errors.Select(e => e.Description));
+                throw new Exception($"Failed to create admin user: {errors}");
+            }
+        }
     }
 }
